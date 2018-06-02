bd21ce0b5767f14600a196812c393e47da6d2d62
Susana Han <susanahan@ac.c4q.nyc>
2018-06-02 03:10:26 -0400

    first commit setting up webpack

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>My React App!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
