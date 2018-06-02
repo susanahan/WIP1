d4a7d74e64834ad436a537adc872aba43425bd48
Susana Han <susanahan@ac.c4q.nyc>
2018-06-02 12:35:17 -0400

    modified and delete webpack files to work with App. Messed around with the package.json and addinged yarn

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import {Route, Link} from "react-router-dom";
+import Welcome from "./Home/Welcome";
+import Particles from 'react-particles-js';
+// import Users from './components/Users/Users';
+// import QuestionsForm from './components/Questions/Questions';
+import HeaderNav from "./Nav/Header";
+
+import FooterNav from "./Nav/Footer";
+import './styles/sticky-footer.css';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <HeaderNav />
         <p>My React App!</p>
+
+        <Route exact path="/" component={Welcome} />
+        <Route path="/users" component={""} />
+        <Route exact path="/user/questions" component={""} />
+        <FooterNav />
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './public/index.html',
+      favicon: 'public/favicon.ico'      
     })
   ]
 };
