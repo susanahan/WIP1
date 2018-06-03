ad8e8abbcf2ee02a7aea564387a066a8ae089d8c
Susana Han <susanahan@ac.c4q.nyc>
2018-06-03 12:56:01 -0400

    removing node_modules

--- client/src/components/Questions/Questions.js
+++ client/src/components/Questions/Questions.js
@@ -11,17 +11,16 @@ class QuestionsForm extends Component {
             questions: [],
             questionId: '',
             user_id: '',
-            questionIndex:0
+            questionIndex:0,
+            selectedOption: ''
         };
-      this.handleRadioChange = this.handleRadioChange.bind(this);
     }
 
-    handleRadioChange(e) {
+    handleRadioChange = (e) => {
         let input = e.target.value;
         this.setState({
-            questionId: input
+            selectedOption: input
         });
-       console.log('questionId', this.state.questionId);
     };
 
     componentDidMount() {
@@ -29,43 +28,49 @@ class QuestionsForm extends Component {
             .get('/users/questions')
             .then((res) => {
                 console.log('res' , res.data.data)
-                    this.setState({questions: res.data.data})
+                this.setState({questions: res.data.data})
             })
             .catch((err) => {
                 console.log(err);
             })
     }
-
+    nextQuestion = () =>{
+        if(questionIndex === 0){
+            this.setState({
+                questionIndex: null               
+            })
+        }else{
+            this.setState({
+                questionIndex: questionIndex + 1               
+            })
+        }    
+    }
+    
     render() {
-        const { questions, options, questionIndex } = this.state;
+        const { questions, options, questionIndex, selectedOption } = this.state;
         //console.log('this.state.questions', questions);
         const questionEls = questions.map((q) =>{
         return(
          <div>
-             <FormGroup>
-         <h1 className='h2FormQuestions'>{q.question_text}</h1>
-         <Radio value='option1' onChange={this.handleRadioChange}>{q.answer_option1}</Radio>
-         <Radio value='option2' onChange={this.handleRadioChange}>{q.answer_option2}</Radio>
-         <Radio value='option3' onChange={this.handleRadioChange}>{q.answer_option3}</Radio>
-         <Radio>{q.answer_option4}</Radio>
-         <Radio>{q.answer_option5}</Radio>
-
+         <FormGroup>
+            <h1 className='h2FormQuestions'>{q.question_text}</h1>
+            <Radio value='option1'  checked={selectedOption === 'option1'} onChange={this.handleRadioChange}>{q.answer_option1}</Radio>
+            <Radio value='option2'  checked={selectedOption === 'option2'} onChange={this.handleRadioChange}>{q.answer_option2}</Radio>
+            <Radio value='option3'  checked={selectedOption === 'option3'} onChange={this.handleRadioChange}>{q.answer_option3}</Radio>
+            <Radio value='option4'  checked={selectedOption === 'option4'} onChange={this.handleRadioChange}>{q.answer_option4}</Radio>
+            <Radio value='option5'  checked={selectedOption === 'option5'} onChange={this.handleRadioChange}>{q.answer_option5}</Radio>
          </FormGroup>
          </div>
-     )
-
- })
-
+        )
+    })
         return (
             <div className='flex-container'>
                 <Jumbotron bsClass='formCustom'>
-               
                 {questionEls[questionIndex]}
-                 <button onClick={() => {this.setState({questionIndex: questionIndex + 1})}}> Next </button>
+                 <button onClick={this.nextQuestion}> Next </button>
                 </Jumbotron>
             </div>
         );
-
     }
 }
 
